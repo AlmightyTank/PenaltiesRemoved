7835d3b396198d1a6de015690e3294648a9a74da
AMightyTank <chrisyoungplus@outlook.com>
2025-05-24 01:05:35 -0500

    Updated to 3.11

--- src/mod.js
+++ src/mod.js
@@ -1,70 +1,25 @@
-import { DependencyContainer } from "tsyringe";
-
-import { jsonc } from "jsonc";
-import path from "path";
-import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
-import { ILogger } from "@spt/models/spt/utils/ILogger";
-import { DatabaseServer } from "@spt/servers/DatabaseServer";
-import { FileSystemSync } from "@spt/utils/FileSystemSync";
-import { jsonc } from "jsonc";
-import { ItemHelper } from "@spt/helpers/ItemHelper";
-import { BaseClasses } from "@spt/models/enums/BaseClasses";
-
-// #region ModConfig 
-interface ModConfig {
-    Equipment: Equipment;
-    Weapons: Weapons;
-    Ammo: Ammo;
-}
-
-interface Weapons {
-    Enabled: boolean;
-    RemoveErgoPenalty: boolean;
-    RemoveRecoilPenalty: boolean;
-    RemoveAccuracyPenalty: boolean;
-    RemoveVelocityPenalty: boolean;
-    RemoveMuzzleOverheatingPenalty: boolean;
-    RemoveDurabilityBurnPenalty: boolean;
-}
-
-interface Equipment {
-    Enabled: boolean;
-    RemoveErgoPenalty: boolean;
-    RemoveTurnPenalty: boolean;
-    RemoveMovePenalty: boolean;
-    RemoveHearingPenalty: boolean;
-    AudioDistortionModifier: number;
-    AmbientNoiseOffsetAmount: number;
-}
-
-interface Ammo {
-    Enabled: boolean;
-    MisfireMultiplier: number;
-    RemoveMuzzleOverheatingPenalty: boolean;
-    RemoveDurabilityBurnPenalty: boolean;
-    RemoveAmmoRecoilPenalty: boolean;
-    RemoveAmmoAccuracyPenalty: boolean;
-}
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonc_1 = require("C:/snapshot/project/node_modules/jsonc");
+const path_1 = __importDefault(require("path"));
+const BaseClasses_1 = require("C:/snapshot/project/obj/models/enums/BaseClasses");
 // #endregion
-
-class PenaltiesRemoved implements IPostDBLoadMod {
-    private modConfig: ModConfig;
-    private logger: ILogger;
-
-    public postDBLoad(container: DependencyContainer): void {
-        const fileSystem = container.resolve<FileSystemSync>("FileSystemSync");
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-        const itemHelper = container.resolve<ItemHelper>("ItemHelper");
-
-        this.logger = container.resolve<ILogger>("WinstonLogger");
-        
-        this.modConfig = jsonc.parse(fileSystem.read(path.resolve(__dirname, "../config/config.jsonc")));
-
+class PenaltiesRemoved {
+    modConfig;
+    logger;
+    postDBLoad(container) {
+        const fileSystem = container.resolve("FileSystemSync");
+        const databaseServer = container.resolve("DatabaseServer");
+        const itemHelper = container.resolve("ItemHelper");
+        this.logger = container.resolve("WinstonLogger");
+        this.modConfig = jsonc_1.jsonc.parse(fileSystem.read(path_1.default.resolve(__dirname, "../config/config.jsonc")));
         const items = databaseServer.getTables().templates.items;
-
         for (const itemId in items) {
             const item = items[itemId];
-            if (this.modConfig.Weapons.Enabled && itemHelper.isOfBaseclass(itemId, BaseClasses.MOD)) {
+            if (this.modConfig.Weapons.Enabled && itemHelper.isOfBaseclass(itemId, BaseClasses_1.BaseClasses.MOD)) {
                 if (this.modConfig.Weapons.RemoveErgoPenalty && item._props.Ergonomics < 0) {
                     item._props.Ergonomics = 0;
                 }
@@ -80,7 +35,8 @@ class PenaltiesRemoved implements IPostDBLoadMod {
                 if (this.modConfig.Weapons.RemoveDurabilityBurnPenalty && item._props.DurabilityBurnModificator > 1.0) {
                     item._props.DurabilityBurnModificator = 1.0;
                 }
-            } else if (this.modConfig.Ammo.Enabled && itemHelper.isOfBaseclass(itemId, BaseClasses.AMMO)) {
+            }
+            else if (this.modConfig.Ammo.Enabled && itemHelper.isOfBaseclass(itemId, BaseClasses_1.BaseClasses.AMMO)) {
                 if (this.modConfig.Ammo.MisfireMultiplier && item._props.MisfireChance != 0) {
                     item._props.MisfireChance *= this.modConfig.Ammo.MisfireMultiplier;
                 }
@@ -96,8 +52,9 @@ class PenaltiesRemoved implements IPostDBLoadMod {
                 if (this.modConfig.Ammo.RemoveMuzzleOverheatingPenalty && item._props.HeatFactor > 1.0) {
                     item._props.HeatFactor = 1.0;
                 }
-            } else if (this.modConfig.Equipment.Enabled) {
-                if (itemHelper.isOfBaseclass(itemId, BaseClasses.HEADPHONES)) {
+            }
+            else if (this.modConfig.Equipment.Enabled) {
+                if (itemHelper.isOfBaseclass(itemId, BaseClasses_1.BaseClasses.HEADPHONES)) {
                     if (this.modConfig.Equipment.AudioDistortionModifier != 1.0) {
                         item._props.Distortion *= this.modConfig.Equipment.AudioDistortionModifier;
                     }
@@ -105,7 +62,7 @@ class PenaltiesRemoved implements IPostDBLoadMod {
                         item._props.AmbientVolume += this.modConfig.Equipment.AmbientNoiseOffsetAmount;
                     }
                 }
-                if (itemHelper.isOfBaseclasses(itemId, [BaseClasses.ARMORED_EQUIPMENT, BaseClasses.VEST, BaseClasses.BACKPACK])) {
+                if (itemHelper.isOfBaseclasses(itemId, [BaseClasses_1.BaseClasses.ARMORED_EQUIPMENT, BaseClasses_1.BaseClasses.VEST, BaseClasses_1.BaseClasses.BACKPACK])) {
                     if (this.modConfig.Equipment.RemoveErgoPenalty) {
                         item._props.weaponErgonomicPenalty = 0;
                     }
@@ -123,5 +80,5 @@ class PenaltiesRemoved implements IPostDBLoadMod {
         }
     }
 }
-
 module.exports = { mod: new PenaltiesRemoved() };
+//# sourceMappingURL=mod.js.map
